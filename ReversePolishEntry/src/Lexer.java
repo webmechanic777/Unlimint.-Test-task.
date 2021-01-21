8ecce3e552ee80fa1029f13f049006c0371e5be5
webmechanic777 <viktorsytin@gmail.com>
2021-01-21 12:38:36 +0300

    Revert "FiniteStateMachine"
    This reverts commit 9ca3adac4c5d2543732d2c784a6fe6cb278399b5.

---
+++ ReversePolishEntry/src/Lexer.java
@@ -0,0 +1,65 @@
+import static java.lang.Double.parseDouble;
+
+public class Lexer {
+    private final String expression;
+    private int currentPosition = 0;
+
+    public Lexer(String expression) {
+        this.expression = expression;
+    }
+
+    public Lexem getNextToken() {//получить лексему, переключает currentPosition
+        return accomplishment();
+    }
+
+    public Lexem lookAhead() {//посмотреть следующую лексему, не переключает currentPosition
+        int IncrementOff = currentPosition;
+        Lexem lexem = accomplishment();
+        currentPosition = IncrementOff;
+        return lexem;
+    }
+    private Lexem accomplishment() {
+        Lexem lexem = new Lexem("", LexemType.EOF);
+        if (currentPosition < expression.length()) {
+            if (Character.isDigit(expression.charAt(currentPosition))) {
+                lexem = parseNumber();
+            } else {//если s +-/*^()
+                lexem = parseNotANumber();
+                currentPosition++;
+            }
+            return lexem;//возвращаем или цифры или +-/*^()
+        }
+        return lexem;//если currentPosition !< expression.length()
+    }
+
+    private Lexem parseNumber() {
+        StringBuilder s = new StringBuilder();
+        while (currentPosition < expression.length() && Character.isDigit(expression.charAt(currentPosition))) {
+                s.append(expression.charAt(currentPosition));
+                currentPosition++;
+        }
+        return new Lexem(parseDouble(s.toString()), LexemType.NUMBER);
+    }
+
+    private Lexem parseNotANumber() {
+        switch (expression.charAt(currentPosition)) {
+            case '+':
+                return new Lexem("+", LexemType.PLUS);
+            case '-':
+                return new Lexem("-", LexemType.MINUS);
+            case '*':
+                return new Lexem("*", LexemType.MULTIPLY);
+            case '/':
+                return new Lexem("/", LexemType.DIVISION);
+            case '^':
+                return new Lexem("^", LexemType.DEGREE);
+            case '(':
+                return new Lexem("(", LexemType.LEFT_BRACKET);
+            case ')':
+                return new Lexem(")", LexemType.RIGHT_BRACKET);
+            default:
+                return new Lexem("", LexemType.EOF);
+        }
+    }
+}
+
---
+++ ReversePolishEntry/src/StackArray.java
@@ -0,0 +1,41 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+public class StackArray<E> implements MyStack<E> {
+    private E[] arr;
+
+    public StackArray(Class<E> type) {
+        this.arr = (E[]) Array.newInstance(type, 0);
+    }
+
+    @Override
+    public void push(E e) {//Помещает элемент в верхнюю часть этой стопки.
+        arr = Arrays.copyOf(arr, arr.length + 1);//добавить увеличение размера
+        arr[arr.length - 1] = e;
+    }
+
+    @Override
+    public E pop() {//Удаляет объект наверху этого стека и возвращает этот объект как значение этой функции.
+        E e = arr[arr.length - 1];
+        arr = Arrays.copyOf(arr, arr.length - 1);
+        return e;
+    }
+
+    @Override
+    public E peek() {//Смотрит на объект вверху этого стека, не удаляя его из стека.
+        if (arr.length == 0)
+            return null;//throw new NoSuchElementException();
+        E e = arr[arr.length - 1];
+        return e;
+    }
+
+    @Override
+    public int size() {
+        return arr.length;
+    }
+
+    public String toString() {
+        return Arrays.toString(arr);
+    }
+}
